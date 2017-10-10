49d1d2c9e7c7e51b2cbbfd7d66138250b02793c9
prayad <32671295+prayad@users.noreply.github.com>
2017-10-10 13:22:57 +0100

    Update json.php

--- json.php
+++ json.php
@@ -1,12 +1,8 @@
 <?php
 $items = array();
 for($i=0;$i<10;$i++){
-$items[$i]="This message $i";}
-header('content-Type: application/json');
-$jasonOutput=json_encode($items);echo "# test1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/prayad/test1.git
-git push -u origin master
+$items[$i]="This message $i";
+}
+header('Content-Type: application/json');
+$jsonOutput=json_encode($items);
 echo $jsonOutput;
